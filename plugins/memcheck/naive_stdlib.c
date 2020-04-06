097189d8461e80815b22a7f872d4b64a66e45d45
Cosmin Gorgovan <cosmin@linux-geek.org>
2020-04-06 16:19:30 +0100

    memcheck_strncmp(): handle n=0 correctly

--- plugins/memcheck/naive_stdlib.c
+++ plugins/memcheck/naive_stdlib.c
@@ -97,6 +97,9 @@ size_t memcheck_strlen(const char *s) {
 
 int memcheck_strncmp(const char *s1, const char *s2, size_t n) {
   size_t i = 0;
+
+  if (n == 0) return 0;
+
   while(s1[i] == s2[i] && s1[i] != '\0' && (i+1) < n) {
     i++;
   }
