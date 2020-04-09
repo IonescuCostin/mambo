6f1170dcef6ead6828db8fa52b9a00e0bdce9c5c
Cosmin Gorgovan <cosmin@linux-geek.org>
2020-04-09 19:42:41 +0100

    mambo_set_source_addr(): clear LSB to handle thumb pointers correctly

--- api/plugin_support.c
+++ api/plugin_support.c
@@ -273,7 +273,7 @@ int mambo_set_source_addr(mambo_context *ctx, void *source_addr) {
   }
 #endif
 
-  ctx->code.read_address = (void *)source_addr;
+  ctx->code.read_address = (void *)((uintptr_t)source_addr & ~1);
   ctx->code.replace = true;
 
   return 0;
