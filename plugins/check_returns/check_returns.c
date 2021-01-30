6c5bbbea6f7e651e0cc42fb388bd5291a738301d
Ionescu Costin <ionescucostin@outlook.com>
2021-01-30 13:10:51 +0000

    Code Cleanup

--- plugins/check_returns/check_returns.c
+++ plugins/check_returns/check_returns.c
@@ -27,9 +27,10 @@ int count;
 //------------------------------------------------------------------------------
 //Function used to check if the branch address is a valid function address
 void check_address(void* addr){
-  // fprintf(stderr, "Return to: %lX\n", addr);
+
   uintptr_t value;
   int ret = mambo_ht_get(&valid_addresses, (uintptr_t)addr, &value);
+  
   if(ret != 0){;
     fprintf(stderr, "Encountered invalid return to %lX\n", addr);
     exit(EXIT_FAILURE);
@@ -40,9 +41,8 @@ void check_address(void* addr){
 // is valid
 int check_returns_pre_inst_handler(mambo_context *ctx) {
   mambo_branch_type type = mambo_get_branch_type(ctx);
-
+  
   if(type & BRANCH_RETURN){
-    // fprintf(stderr, "Branch from %lX", mambo_get_source_addr(ctx));
     emit_push(ctx, (1 << reg0));
     emit_mov(ctx, reg0, lr);
     emit_safe_fcall(ctx, check_address, 1);
@@ -81,7 +81,7 @@ int check_returns_pre_thread(mambo_context *ctx) {
         char *token = strtok(buffer, " \t");
         token[strlen(token) - 1] = '\0';
         uint32_t ret_addr = (int)strtol(token, NULL, 16) + 4;
-
+        
         uintptr_t value;      
         ret = mambo_ht_get(&valid_addresses, ret_addr, &value);
         if(ret != 0){
@@ -100,26 +100,21 @@ int check_returns_pre_thread(mambo_context *ctx) {
 
 
 // Virtual memory operation callbacks
-// int check_returns_vm_op_handler(mambo_context *ctx) {
-//   vm_op_t type = mambo_get_vm_op(ctx);
+int check_returns_vm_op_handler(mambo_context *ctx) {
+  vm_op_t type = mambo_get_vm_op(ctx);
 
-//   int addr = mambo_get_vm_addr(ctx);
-//   int size = mambo_get_vm_size(ctx);
-//   int filedes = mambo_get_vm_filedes(ctx);
-//   int prot = mambo_get_vm_prot(ctx);
+  int addr = mambo_get_vm_addr(ctx);
+  int size = mambo_get_vm_size(ctx);
+  int filedes = mambo_get_vm_filedes(ctx);
+  int prot = mambo_get_vm_prot(ctx);
 
-//   if((type == VM_MAP) && (filedes > 0) && ((prot & PROT_EXEC) == PROT_EXEC)){
-//     printf("%d \n", filedes);
-//     printf("%d \n", prot&PROT_EXEC);
-//     printf("%X \n", mambo_get_vm_flags(ctx));
-//     printf("addr: %X\nsize: %d\n\n", addr, size);
-//   }
-
-//   // if(result == VM_MAP &&  > 0 && ( & PROT_EXEC) == 1){
-//   //   printf("addr: %X\nsize: %d\n\n", addr, size);
-//   //   count++;
-//   // }
-// }
+  if((type == VM_MAP) && (filedes > 0) && ((prot & PROT_EXEC) == PROT_EXEC)){
+    printf("%d \n", filedes);
+    printf("%d \n", prot&PROT_EXEC);
+    printf("%X \n", mambo_get_vm_flags(ctx));
+    printf("addr: %X\nsize: %d\n\n", addr, size);
+  }
+}
 
 // Exit callback
 int check_returns_exit_handler(mambo_context *ctx) {
@@ -138,13 +133,13 @@ __attribute__((constructor)) void check_returns_init_plugin() {
   int ret = mambo_register_pre_inst_cb(ctx, &check_returns_pre_inst_handler);
   assert(ret == MAMBO_SUCCESS);
 
-  // count = 0;
-  // ret = mambo_register_vm_op_cb(ctx, &check_returns_vm_op_handler);
-  // assert(ret == MAMBO_SUCCESS);
+  count = 0;
+  ret = mambo_register_vm_op_cb(ctx, &check_returns_vm_op_handler);
+  assert(ret == MAMBO_SUCCESS);
   
   ret = mambo_register_pre_thread_cb(ctx, &check_returns_pre_thread);
   assert(ret == MAMBO_SUCCESS);
 
-  // ret = mambo_register_exit_cb(ctx, &check_returns_exit_handler);
-  // assert(ret == MAMBO_SUCCESS);
+  ret = mambo_register_exit_cb(ctx, &check_returns_exit_handler);
+  assert(ret == MAMBO_SUCCESS);
 }
